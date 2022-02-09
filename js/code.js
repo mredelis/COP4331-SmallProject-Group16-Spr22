1b725a820f7583f913a6f3fb8e846b25b7166fc5
Edelis <edelitamr@gmail.com>
2022-02-09 15:17:45 +0000

    more to come

--- js/code.js
+++ js/code.js
@@ -135,44 +135,20 @@ function addContact() {
     }
 }
 
-function searchContact() {
+// Edelis work in progress
+function doLoadContacts() {
+	//readCookie() is on the search.html so the userId should be available when <body> loads
+
+	let searchString = document.getElementById("searchBar").value;
+
+	console.log("UserID: ", userId);
+	console.log("Search String: ", searchString);
+
+	let temp = {userID: userId, search: searchString};
+	// let jsonPayload = 
 
 
 }
-
-
-// function saveCookie() {
-// 	let minutes = 20;
-// 	let date = new Date();
-// 	date.setTime(date.getTime() + (minutes * 60 * 1000));
-// 	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
-// }
-
-// function readCookie() {
-// 	userId = -1;
-// 	let data = document.cookie;
-// 	let splits = data.split(",");
-// 	for (var i = 0; i < splits.length; i++) {
-// 		let thisOne = splits[i].trim();
-// 		let tokens = thisOne.split("=");
-// 		if (tokens[0] == "firstName") {
-// 			firstName = tokens[1];
-// 		}
-// 		else if (tokens[0] == "lastName") {
-// 			lastName = tokens[1];
-// 		}
-// 		else if (tokens[0] == "userId") {
-// 			userId = parseInt(tokens[1].trim());
-// 		}
-// 	}
-
-// 	if (userId < 0) {
-// 		window.location.href = "index.html";
-// 	}
-// 	else {
-// 		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
-// 	}
-// }
 
 
 /* MOVED TO A register.js FILE */
