2787ac756c3dd92f663eaeabdf0e3c4ac739207c
Edelis <edelitamr@gmail.com>
2022-02-09 03:29:56 +0000

    empty str in seachContacts returns all contacts

--- LAMPAPI/SearchContact.php
+++ LAMPAPI/SearchContact.php
@@ -1,87 +1,78 @@
 <?php
-	// Get info as PHP object
-   	$inData = getRequestInfo();
 
-	$search = $inData["search"];
-   	$userID = $inData["userID"];
+$inData = getRequestInfo();
 
-	// searchResults will be a stringified, comma-separated list of JSON objects
-   	$searchResults = "";
-   	$searchCount = 0;
+// $search = $inData["search"];
+$userID = $inData["userID"];
+$str = "%" . $inData["search"] . "%"; //using wildcards
 
-	$conn = new mysqli("localhost", "user16", "Group16", "COP4331");
+$searchResults = "";
+$searchCount = 0;
 
-	if ($conn->connect_error)
-	{
-		returnWithError($conn->connect_error);
-	}
-	else
-	{
-		// Query to locate matching rows
-		// Matches on any first or last name containing
-		// the search string at any position
+$conn = new mysqli("localhost", "user16", "Group16", "COP4331");
+
+if ($conn->connect_error) {
+	returnWithError($conn->connect_error);
+} else {
+	// If empty string in the search field, return all contact of userID
+	if ($inData["search"] == "") {
+		$stmt = $conn->prepare("SELECT ID,FirstName,LastName,Email,Phone FROM Contacts WHERE UserID=?");
+		$stmt->bind_param("i", $userID);
+	} else {
 		$stmt = $conn->prepare("SELECT ID,FirstName,LastName,Email,Phone FROM Contacts WHERE (FirstName LIKE ? OR LastName LIKE ?) AND UserID=?");
-		$search = "%" . $search . "%";
-		$stmt->bind_param("sss", $search, $search, $userID);
-		$stmt->execute();
-		$result = $stmt->get_result();
+		$stmt->bind_param("ssi", $str, $str, $userID);
+	}
 
-		while ($row = $result->fetch_assoc())
-		{
-			if ($searchCount > 0)
-			{
-				$searchResults .= ",";
-			}
+	$stmt->execute();
+	$result = $stmt->get_result();
 
-			$searchCount++;
-
-			// Append current row as single JSON object
-			// I'm pretty sure this is wildly inefficient
-			// and would scale horribly
-			$searchResults .= '{';
-			$searchResults .= '"id":' . $row["ID"] . ',';
-			$searchResults .= '"firstName":"' . $row["FirstName"] . '",';
-			$searchResults .= '"lastName":"' . $row["LastName"] . '",';
-			$searchResults .= '"email":"' . $row["Email"] . '",';
-			$searchResults .= '"phone":"' . $row["Phone"]. '"';
-			$searchResults .= '}';
+	while ($row = $result->fetch_assoc()) {
+		if ($searchCount > 0) {
+			$searchResults .= ",";
 		}
-
-		if ($searchCount == 0)
-		{
-			returnWithError("No Records Found");
-		}
-		else
-		{
-			returnWithInfo($searchResults);
-		}
-
-		$stmt->close();
-		$conn->close();
+		$searchCount++;
+		$searchResults .= '{';
+		$searchResults .= '"id":' . $row["ID"] . ',';
+		$searchResults .= '"firstName":"' . $row["FirstName"] . '",';
+		$searchResults .= '"lastName":"' . $row["LastName"] . '",';
+		$searchResults .= '"email":"' . $row["Email"] . '",';
+		$searchResults .= '"phone":"' . $row["Phone"] . '"';
+		$searchResults .= '}';
 	}
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
+	if ($searchCount == 0) {
+		returnWithError("No Records Found");
+	} else {
+		returnWithInfo($searchResults);
 	}
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
+	$stmt->close();
+	$conn->close();
+}
 
- 	function returnWithInfo( $searchResults )
-	{
-		// Return search results as array of JSON objects
-		$retValue = '{"results":[' . $searchResults . '],"error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
 
-?>
+
+function getRequestInfo()
+{
+	return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj)
+{
+	header('Content-type: application/json');
+	echo $obj;
+}
+
+
+function returnWithError($err)
+{
+	$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+	sendResultInfoAsJson($retValue);
+}
+
+function returnWithInfo($searchResults)
+{
+	// Return search results as array of JSON objects
+	$retValue = '{"results":[' . $searchResults . '],"error":""}';
+	sendResultInfoAsJson($retValue);
+}
